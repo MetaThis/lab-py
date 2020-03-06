735d10ce1eb797cb5aca712efbc2a7cf27cf91c1
David Jacobs <citeon@gmail.com>
2020-03-05 20:29:54 -0600

    Saving submitted data to sqlite

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import flask
 from flask import request, jsonify
 import fastjsonschema
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -21,7 +22,6 @@ validate = fastjsonschema.compile({
 
 @app.errorhandler(fastjsonschema.exceptions.JsonSchemaException)
 def handle_invalid_data(error):
-    print(error)
     response = jsonify({"error": error.message})
     return response, 400
 
@@ -29,8 +29,27 @@ def handle_invalid_data(error):
 def post_samples(instrument_id):
     payload = request.json
     samples = validate(payload)
-    print(samples)
-    run = {"runId": 1}
-    return run
+    run_id = save(instrument_id, samples)
+    return {"runId": run_id}
+
+def save(instrument_id, samples):
+    conn = sqlite3.connect('lab.db')
+    c = conn.cursor()
+
+    # Insert run
+    c.execute('INSERT INTO run_instrument(instrument_id) VALUES (?)', (instrument_id,))
+
+    # Retrieve run id
+    c.execute('SELECT last_insert_rowid() AS id')
+    run_id = c.fetchone()[0]
+
+    # Insert samples belonging to run
+    data = list(map(lambda x: (x["id"], run_id), samples))
+    c.executemany('INSERT INTO run_sample(sample_id, run_id) VALUES (?, ?)', data)
+	
+    conn.commit()
+    conn.close
+
+    return run_id
 
 app.run()
